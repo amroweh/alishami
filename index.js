f70f8d3eae8cafa3938aa532512bc2e0db24cc71
Ali <amroweh@gmail.com>
2023-06-06 23:30:10 +0100

    added responsive menu (needs styling)

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function toggleProjectMenu(){
     else projectTitles.style.display = "flex"
 }
 
+function menuToggle(){
+    let x = document.querySelector("header nav");    
+    if (x.classList.contains("responsive")) x.classList.remove("responsive")              
+    else x.classList.add("responsive") 
+}
+
 projectToggleButton.addEventListener("click", toggleProjectMenu)
